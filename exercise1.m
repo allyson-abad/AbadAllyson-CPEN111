1bcab8e427f0e2ce207815d82b68180d7cdeea26
Allyson Abad <110139900+allyson-abad@users.noreply.github.com>
2022-11-23 09:49:30 +0800

    Add files via upload

---
+++ exercise1.m
@@ -0,0 +1,21 @@
+pkg load image;
+
+fruits = imread('fruits.png');                   % Read the image information
+subplot(311);                                    % Positioning the image
+imshow(fruits);
+title('Original Fruits Image')
+
+fruits2 = imresize(imresize(fruits,1/20),20);    % Resizing the picture
+subplot(312);                                    % Positioning the image
+imshow(fruits2);
+title('Reduced Resolution of Fruits Image');
+imwrite(fruits2,'fruits2.png');                  % Saving the image as fruits2.png
+
+fruits3 = rgb2hsv(fruits);                       % Change the color of the picture to HSV
+subplot(313)                                     % Positioning the image
+imshow(fruits3);                                 % Display converted version (HSV)
+title('HSV Color of Fruits Image');
+imwrite(fruits3,'fruits3.png');                  % Saving the image as fruits3.png
+
+
+
